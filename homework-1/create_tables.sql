be2c7001b5e2136b1cfb34195636257e6616f6fb
Aleksey Belyaev <lesha.belyaev.73@mail.ru>
2023-12-09 21:45:05 +0300

    Создал три таблицы в базе данных код перенес в файл create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	customer_id varchar PRIMARY KEY,
+	company_name varchar(50) NOT NULL,
+	contact_name varchar(30) NOT NULL
+);
+
+CREATE TABLE customers
+(
+	employee_id serial PRIMARY KEY,
+	first_name varchar(20) NOT NULL,
+	last_name varchar(20) NOT NULL,
+	title text,
+	birth_date date NOT NULL,
+	notes text
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(5) REFERENCES employees(customer_id) NOT NULL,
+	employee_id int REFERENCES customers(employee_id)NOT NULL,
+	order_date date NOT NULL,
+	ship_city text
+);
+
