5ad1dc420c88d06c3ea379ef755de1a24eaa4511
Aleksey Belyaev <lesha.belyaev.73@mail.ru>
2023-12-10 21:43:03 +0300

    Исправил код по созданию таблиц и написал код по добавлению данных в таблицу employee

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,12 +1,12 @@
 -- SQL-команды для создания таблиц
-CREATE TABLE employees
+CREATE TABLE customers
 (
 	customer_id varchar PRIMARY KEY,
 	company_name varchar(50) NOT NULL,
 	contact_name varchar(30) NOT NULL
 );
 
-CREATE TABLE customers
+CREATE TABLE employees
 (
 	employee_id serial PRIMARY KEY,
 	first_name varchar(20) NOT NULL,
@@ -19,8 +19,8 @@ CREATE TABLE customers
 CREATE TABLE orders
 (
 	order_id int PRIMARY KEY,
-	customer_id varchar(5) REFERENCES employees(customer_id) NOT NULL,
-	employee_id int REFERENCES customers(employee_id)NOT NULL,
+	customer_id varchar(5) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id)NOT NULL,
 	order_date date NOT NULL,
 	ship_city text
 );
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,27 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="postgres",
+    port="5433",
+)
+with open("north_data/employees_data.csv", "r", newline="") as file:
+    employees = csv.reader(file)
+    next(employees)
+    with conn.cursor() as cur:
+        for employer in employees:
+            for employee in employees:
+                cur.execute(
+                    "INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes) VALUES (%s, %s, %s, %s, %s, %s)",
+                    employee,
+                )
+                cur.execute("SELECT * FROM employees")
+                rows = cur.fetchall()
+print(rows)
+
+conn.close()
